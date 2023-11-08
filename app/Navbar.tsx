73124ee4ff3a3212ad645308ec0fe20ef754ccd8
UnicornDad8 <unicorndad8@gmail.com>
2023-11-08 15:17:08 -0300

    Building the navbar component and put it on top of layout

---
+++ app/Navbar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Link from "next/link";
+import { BsFillBugFill } from "react-icons/bs";
+
+const Navbar = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <BsFillBugFill size={"1.8rem"} />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
