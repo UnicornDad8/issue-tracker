302ba3f90b31387e7d60ade62e82221670fe90a9
UnicornDad8 <unicorndad8@gmail.com>
2023-11-14 19:28:59 -0300

    Handling form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,30 +1,64 @@
 "use client";
 
 import React from "react";
+import { useRouter } from "next/navigation";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import styles from "./style.module.css";
 
+interface IssueForm {
+  title: string;
+  description: string;
+}
+
 const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+
   return (
-    <div className="max-w-xl space-y-3">
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        try {
+          await axios.post("/issues/new", data, {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+          router.push("/issues");
+        } catch (error) {
+          console.log(error);
+        }
+      })}
+    >
       <div className="leading-normal">
         <input
           type="text"
           className="border rounded-sm w-full px-4 py-3"
           placeholder="Title"
+          {...register("title")}
         />
       </div>
       <div className="leading-normal">
-        <SimpleMDE
-          className="border rounded-sm w-full px-4 py-3"
-          placeholder="Description"
+        <Controller
+          name="Description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE
+              className="border rounded-sm w-full px-4 py-3"
+              placeholder="Description"
+              {...field}
+              ref={null}
+            />
+          )}
         />
       </div>
       <button className={`${styles["btn"]} ${styles["btnBlue"]}`}>
         Submit New Issue
       </button>
-    </div>
+    </form>
   );
 };
 
