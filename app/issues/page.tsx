c3fe269f7e34a13299601a7219ab66b52b1c40e9
UnicornDad8 <unicorndad8@gmail.com>
2023-11-17 16:09:48 -0300

    Show the issues in a table

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,13 +1,58 @@
 import React from "react";
 import Link from "next/link";
+import prisma from "@/prisma/client";
 import styles from "./style.module.css";
 
-const IssuesPage = () => {
+const IssuesPage = async () => {
+  const issues = await prisma.issue.findMany();
+
   return (
     <div>
-      <button className={`${styles["btn"]} ${styles["btnBlue"]}`}>
-        <Link href="/issues/new">New Issue</Link>
-      </button>
+      <div className="mb-5">
+        <button className={`${styles["btn"]} ${styles["btnBlue"]}`}>
+          <Link href="/issues/new">New Issue</Link>
+        </button>
+      </div>
+
+      <div className="relative overflow-x-auto sm:rounded-lg">
+        <table className="w-full border text-sm text-left rtl:text-right text-gray-500 bg-white">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Issue
+              </th>
+              <th scope="col" className="px-6 py-3 hidden md:table-cell">
+                Status
+              </th>
+              <th scope="col" className="px-6 py-3 hidden md:table-cell">
+                Created
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {issues.map((issue) => (
+              <tr
+                key={issue.id}
+                className="odd:bg-white even:bg-gray-50 border-b"
+              >
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-700 whitespace-nowrap"
+                >
+                  {issue.title}
+                  <div className="block md:hidden">{issue.status}</div>
+                </th>
+                <td className="px-6 py-4 hidden md:table-cell">
+                  {issue.status}
+                </td>
+                <td className="px-6 py-4 hidden md:table-cell">
+                  {issue.createdAt.toDateString()}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
