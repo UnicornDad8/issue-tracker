e3cccfa5a197d14d79289d2631da753e0fd7e523
UnicornDad8 <unicorndad8@gmail.com>
2023-11-17 13:29:40 -0300

    Implement issue form client side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -4,18 +4,25 @@ import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
+import { createIssueSchema } from "../../validationSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import styles from "./style.module.css";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const [error, setError] = useState("");
 
   return (
@@ -60,6 +67,9 @@ const NewIssuePage = () => {
             placeholder="Title"
             {...register("title")}
           />
+          {errors.title && (
+            <p className="text-red-500 mt-2">{errors.title.message}</p>
+          )}
         </div>
         <div className="leading-normal">
           <Controller
@@ -74,6 +84,9 @@ const NewIssuePage = () => {
               />
             )}
           />
+          {errors.description && (
+            <p className="text-red-500 mt-2">{errors.description.message}</p>
+          )}
         </div>
         <button className={`${styles["btn"]} ${styles["btnBlue"]}`}>
           Submit New Issue
