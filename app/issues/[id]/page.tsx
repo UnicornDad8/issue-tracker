f5459f731904bcf0e3e76ace586e2a0168403043
UnicornDad8 <unicorndad8@gmail.com>
2023-11-18 16:09:06 -0300

    Styling issue detail page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 
 interface Props {
   params: { id: string };
@@ -17,10 +18,20 @@ const IssueDetailPage = async ({ params }: Props) => {
 
   return (
     <div>
-      <p>{issue.title}</p>
-      <p>{issue.description}</p>
-      <p>{issue.status}</p>
-      <p>{issue.createdAt.toDateString()}</p>
+      <h2 className="text-3xl font-bold">{issue.title}</h2>
+      <div className="flex space-x-3 my-2">
+        <IssueStatusBadge status={issue.status} />
+        <p className="text-gray-900 text-opacity-20">
+          - {issue.createdAt.toDateString()} -
+        </p>
+      </div>
+      <div className="relative border flex flex-col text-gray-700 bg-white shadow-md w-96 rounded-xl bg-clip-border">
+        <div className="p-6">
+          <p className="block text-base antialiased font-light leading-relaxed text-inherit">
+            {issue.description}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
