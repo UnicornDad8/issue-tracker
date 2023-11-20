dc16ae01890425928ee315f291b72d1ec66338c8
UnicornDad8 <unicorndad8@gmail.com>
2023-11-20 08:42:26 -0300

    Added the edit button

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -3,18 +3,23 @@ import { Skeleton } from "@/app/components";
 
 const LoadingIssueDetailPage = () => {
   return (
-    <div className="max-w-xl">
-      <Skeleton width="14rem" />
+    <div className="flex flex-col md:flex-row">
+      <div className="md:flex-1">
+        <Skeleton width="14rem" />
 
-      <div className="flex space-x-3 my-2">
-        <Skeleton width="5rem" />
-        <Skeleton width="8rem" />
-      </div>
-
-      <div className="relative border flex flex-col text-gray-700 bg-white shadow-md w-96 rounded-xl bg-clip-border">
-        <div className="p-6">
-          <Skeleton count={3} />
+        <div className="flex space-x-3 my-2">
+          <Skeleton width="5rem" />
+          <Skeleton width="8rem" />
         </div>
+
+        <div className="relative border flex flex-col text-gray-700 bg-white shadow-md w-96 rounded-xl bg-clip-border">
+          <div className="p-6">
+            <Skeleton count={3} />
+          </div>
+        </div>
+      </div>
+      <div className="md:w-[200px] md:mt-0 mt-4">
+        <Skeleton />
       </div>
     </div>
   );
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import Link from "next/link";
 import ReactMarkdown from "react-markdown";
 import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
 import { IssueStatusBadge } from "@/app/components";
+import { FaRegEdit } from "react-icons/fa";
+import styles from "./style.module.css";
 
 interface Props {
   params: { id: string };
@@ -16,21 +19,33 @@ const IssueDetailPage = async ({ params }: Props) => {
   if (!issue) notFound();
 
   return (
-    <div>
-      <h2 className="text-3xl font-bold">{issue.title}</h2>
-      <div className="flex space-x-3 my-2">
-        <IssueStatusBadge status={issue.status} />
-        <p className="text-gray-900 text-opacity-20">
-          - {issue.createdAt.toDateString()} -
-        </p>
-      </div>
-
-      <div className="relative border flex flex-col text-gray-700 bg-white shadow-md w-96 rounded-xl bg-clip-border">
-        <div className="p-6 prose lg:prose-xl">
-          <ReactMarkdown className="block text-base antialiased leading-relaxed text-inherit">
-            {issue.description}
-          </ReactMarkdown>
+    <div className="flex flex-col md:flex-row">
+      <div className="md:flex-1 w-full">
+        <h2 className="text-3xl font-bold">{issue.title}</h2>
+        <div className="flex space-x-3 my-2">
+          <IssueStatusBadge status={issue.status} />
+          <p className="text-gray-900 text-opacity-20">
+            - {issue.createdAt.toDateString()} -
+          </p>
         </div>
+
+        <div className={styles["card"]}>
+          <div className="p-6 prose lg:prose-xl">
+            <ReactMarkdown className={styles["markdown"]}>
+              {issue.description}
+            </ReactMarkdown>
+          </div>
+        </div>
+      </div>
+      <div className="md:w-[200px] md:mt-0 mt-4 w-full">
+        <Link href={`/issues/${issue.id}/edit`} passHref legacyBehavior>
+          <button
+            className={`${styles["btn"]} ${styles["btnBlue"]} flex items-center`}
+          >
+            <FaRegEdit className="mr-2" />
+            Edit Issue
+          </button>
+        </Link>
       </div>
     </div>
   );
