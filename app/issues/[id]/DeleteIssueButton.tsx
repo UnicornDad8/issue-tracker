3e97eb173a60adffc78fd1aef5f9b90de3f3f8b4
UnicornDad8 <unicorndad8@gmail.com>
2023-11-24 13:14:56 -0300

    Handle errors on delete issue modal

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,19 +1,31 @@
 "use client";
 
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import axios from "axios";
 import { MdDelete } from "react-icons/md";
-import DeleteModal from "./DeleteModal";
+import DeleteModal from "../_components/DeleteModal";
 import { useRouter } from "next/navigation";
 import styles from "./style.module.css";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
-  const modalRef = useRef(null);
+  const modalRefIssue = useRef(null);
+  const modalRefError = useRef(null);
   const router = useRouter();
+
+  const deleteIssue = async () => {
+    try {
+      await axios.delete("/api/issues/" + issueId);
+      router.push("/issues");
+      router.refresh();
+    } catch (error) {
+      modalRefError.current.open();
+      modalRefIssue.current.close();
+    }
+  };
 
   return (
     <>
-      <DeleteModal ref={modalRef} onClose={() => modalRef.current.close()}>
+      <DeleteModal ref={modalRefIssue}>
         <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
           <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
             <div className="sm:flex sm:items-start">
@@ -54,18 +66,14 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
             <button
               type="button"
               className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
-              onClick={async () => {
-                await axios.delete("/api/issues/" + issueId);
-                router.push("/issues");
-                router.refresh();
-              }}
+              onClick={deleteIssue}
             >
               Delete Issue
             </button>
             <button
               type="button"
               className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-              onClick={() => modalRef.current.close()}
+              onClick={() => modalRefIssue.current.close()}
             >
               Cancel
             </button>
@@ -74,12 +82,61 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
       </DeleteModal>
 
       <button
-        onClick={() => modalRef.current.open()}
+        onClick={() => modalRefIssue.current.open()}
         className={`${styles["btn"]} ${styles["btnRed"]} flex items-center flex-1 w-full`}
       >
         <MdDelete className="mr-2" />
         Delete
       </button>
+
+      <DeleteModal ref={modalRefError}>
+        <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+          <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+            <div className="sm:flex sm:items-start">
+              <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                <svg
+                  className="h-6 w-6 text-red-600"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+                  />
+                </svg>
+              </div>
+              <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                <h3
+                  className="text-base font-semibold leading-6 text-gray-900"
+                  id="modal-title"
+                >
+                  Error:
+                </h3>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    This Issue couldn&apos;t be deleted
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+            <button
+              type="button"
+              className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+              onClick={() => {
+                modalRefError.current.close();
+              }}
+            >
+              Ok
+            </button>
+          </div>
+        </div>
+      </DeleteModal>
     </>
   );
 };
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -15,52 +15,54 @@ const IssuesPage = async () => {
         </button>
       </div>
 
-      <div className="relative border overflow-hidden overflow-x-auto sm:rounded-lg">
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 bg-white">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                Issue
-              </th>
-              <th scope="col" className="px-6 py-3 hidden md:table-cell">
-                Status
-              </th>
-              <th scope="col" className="px-6 py-3 hidden md:table-cell">
-                Created
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {issues.map((issue) => (
-              <tr
-                key={issue.id}
-                className="odd:bg-white even:bg-gray-50 border-b"
-              >
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-700 whitespace-nowrap"
-                >
-                  <Link
-                    href={`/issues/${issue.id}`}
-                    className="hover:underline hover:text-blue-700 text-blue-500"
-                  >
-                    {issue.title}
-                  </Link>
-                  <div className="block md:hidden mt-2">
-                    <IssueStatusBadge status={issue?.status} />
-                  </div>
+      {issues.length > 0 && (
+        <div className="relative border overflow-hidden overflow-x-auto sm:rounded-lg">
+          <table className="w-full text-sm text-left rtl:text-right text-gray-500 bg-white">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  Issue
                 </th>
-                <td className="px-6 py-4 hidden md:table-cell">
-                  <IssueStatusBadge status={issue?.status} />
-                </td>
-                <td className="px-6 py-4 hidden md:table-cell">
-                  {issue.createdAt.toDateString()}
-                </td>
+                <th scope="col" className="px-6 py-3 hidden md:table-cell">
+                  Status
+                </th>
+                <th scope="col" className="px-6 py-3 hidden md:table-cell">
+                  Created
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {issues.map((issue) => (
+                <tr
+                  key={issue.id}
+                  className="odd:bg-white even:bg-gray-50 border-b"
+                >
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-700 whitespace-nowrap"
+                  >
+                    <Link
+                      href={`/issues/${issue.id}`}
+                      className="hover:underline hover:text-blue-700 text-blue-500"
+                    >
+                      {issue.title}
+                    </Link>
+                    <div className="block md:hidden mt-2">
+                      <IssueStatusBadge status={issue?.status} />
+                    </div>
+                  </th>
+                  <td className="px-6 py-4 hidden md:table-cell">
+                    <IssueStatusBadge status={issue?.status} />
+                  </td>
+                  <td className="px-6 py-4 hidden md:table-cell">
+                    {issue.createdAt.toDateString()}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
