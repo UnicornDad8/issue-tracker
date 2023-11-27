0665251fea89adc23ba738572d53ec44e0335533
UnicornDad8 <unicorndad8@gmail.com>
2023-11-27 14:39:59 -0300

    Change the layout of the navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -17,39 +17,44 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-      <Link href="/">
-        <BsFillBugFill color="#3B82F6" size={"1.8rem"} />
-      </Link>
-      <ul className="flex">
-        {links.map((link) => (
-          <li
-            key={link.href}
-            className={classnames({
-              "bg-zinc-100": link.href === currentPath,
-              "flex h-14 items-center px-6": true,
-            })}
-          >
-            <Link
-              className={classnames({
-                "text-zinc-900": link.href === currentPath,
-                "text-zinc-500": link.href !== currentPath,
-                "hover:text-zinc-800 transition-colors": true,
-              })}
-              href={link.href}
-            >
-              {link.label}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <div>
-        {status === "authenticated" && (
-          <Link href="/api/auth/signout">Logout</Link>
-        )}
-        {status === "unauthenticated" && (
-          <Link href="/api/auth/signin">Login</Link>
-        )}
+    <nav className="border-b mb-5 px-5">
+      <div className="flex justify-between">
+        <div className="flex items-center gap-x-3">
+          <Link href="/">
+            <BsFillBugFill color="#3B82F6" size={"1.8rem"} />
+          </Link>
+          <ul className="flex">
+            {links.map((link) => (
+              <li
+                key={link.href}
+                className={classnames({
+                  "bg-zinc-100": link.href === currentPath,
+                  "flex h-14 items-center px-6": true,
+                })}
+              >
+                <Link
+                  className={classnames({
+                    "text-zinc-900": link.href === currentPath,
+                    "text-zinc-500": link.href !== currentPath,
+                    "hover:text-zinc-800 transition-colors": true,
+                  })}
+                  href={link.href}
+                >
+                  {link.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="flex items-center">
+          {status === "authenticated" && (
+            <Link href="/api/auth/signout">Logout</Link>
+          )}
+          {status === "unauthenticated" && (
+            <Link href="/api/auth/signin">Login</Link>
+          )}
+        </div>
       </div>
     </nav>
   );
