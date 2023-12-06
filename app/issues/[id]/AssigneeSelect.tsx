6f4ad284ebecdcb01f597d2b2742b4a6a82a946f
UnicornDad8 <unicorndad8@gmail.com>
2023-12-06 11:25:12 -0300

    Show toast notification

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -8,6 +8,7 @@ import { User } from "@prisma/client";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 import { Skeleton } from "@/app/components";
+import toast, { Toaster } from "react-hot-toast";
 
 const key = "assigned-user";
 
@@ -19,8 +20,8 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   useClickOutside(ref, () => setToggle(false));
   useEffect(() => {
     // While loading, load the state from the localStorage.
-    if (window.localStorage.getItem("Value"))
-      setSelectedOption(window.localStorage.getItem("Value"));
+    if (window.localStorage.getItem(key))
+      setSelectedOption(window.localStorage.getItem(key));
   }, []);
 
   const {
@@ -39,57 +40,64 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   if (isLoading) return <Skeleton className="mb-2" height="3rem" />;
 
   const onOptionClicked = (value, userId, i) => async () => {
-    await axios.patch("/api/issues/" + issue.id, {
-      assignedToUserId: userId || null,
-    });
+    try {
+      await axios.patch("/api/xissues/" + issue.id, {
+        assignedToUserId: userId || null,
+      });
 
-    setSelectedOption(value);
-    window.localStorage.setItem("Value", value);
-    setToggle(false);
+      setSelectedOption(value);
+      window.localStorage.setItem(key, value);
+      setToggle(false);
+    } catch (error) {
+      toast.error("Changes could not be saved");
+    }
   };
 
   const usersToMap = [...users, { id: null, name: "Unassigned" }];
 
   return (
-    <div
-      className="w-full mx-auto mb-2 cursor-pointer bg-gray-100"
-      onClick={() => setToggle(!isToggled)}
-    >
-      <button
-        type="button"
-        className={`flex items-center w-full justify-between text-gray-500 border-[1px] focus:border-2 focus:border-blue-500 border-gray-300 py-2 px-4 h-12 min-w-max rounded`}
+    <>
+      <div
+        className="w-full mx-auto mb-2 cursor-pointer bg-gray-100"
+        onClick={() => setToggle(!isToggled)}
       >
-        {selectedOption || "Unassigned"}
-        <IoIosArrowDown
-          size={23}
-          color="#6b7280"
-          className={`transition-transform duration-75 ease-in-out ${
-            isToggled ? "rotate-180" : "rotate-0"
-          }`}
-        />
-      </button>
-      {isToggled && (
-        <div
-          className="w-full my-2 py-2 px-4 mx-auto cursor-pointer flex flex-col divide-y bg-gray-100 border-[1px] border-gray-300 rounded"
-          ref={ref}
+        <button
+          type="button"
+          className={`flex items-center w-full justify-between text-gray-500 border-[1px] focus:border-2 focus:border-blue-500 border-gray-300 py-2 px-4 h-12 min-w-max rounded`}
         >
-          <p className="text-gray-500 text-sm mb-2">Suggestions</p>
-          <ul className="">
-            {usersToMap?.map((user, i) => {
-              return (
-                <li
-                  key={user?.id}
-                  onClick={onOptionClicked(user?.name, user?.id, i)}
-                  className="mt-2 text-gray-600 py-2 px-4 rounded-md hover:bg-gray-300"
-                >
-                  {user?.name}
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      )}
-    </div>
+          {selectedOption || "Unassigned"}
+          <IoIosArrowDown
+            size={23}
+            color="#6b7280"
+            className={`transition-transform duration-75 ease-in-out ${
+              isToggled ? "rotate-180" : "rotate-0"
+            }`}
+          />
+        </button>
+        {isToggled && (
+          <div
+            className="w-full my-2 py-2 px-4 mx-auto cursor-pointer flex flex-col divide-y bg-gray-100 border-[1px] border-gray-300 rounded"
+            ref={ref}
+          >
+            <p className="text-gray-500 text-sm mb-2">Suggestions</p>
+            <ul className="">
+              {usersToMap?.map((user, i) => {
+                return (
+                  <li
+                    key={user?.id}
+                    onClick={onOptionClicked(user?.name, user?.id, i)}
+                    className="mt-2 text-gray-600 py-2 px-4 rounded-md hover:bg-gray-300"
+                  >
+                    {user?.name}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
+      </div>
+      <Toaster />
+    </>
   );
 };
 
