6efda5a3d93a66d212faeb3277e8894287be937e
UnicornDad8 <unicorndad8@gmail.com>
2023-12-11 10:19:46 -0300

    Paginate issues table

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,3 @@
-import { Pagination } from "@/app/components";
-
-export default function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
-  return (
-    <Pagination
-      itemCount={100}
-      pageSize={10}
-      currentPage={parseInt(searchParams.page)}
-    />
-  );
+export default function Home() {
+  return <div>Hello world</div>;
 }
